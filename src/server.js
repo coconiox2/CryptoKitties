2e6e79e9807edcb56622e31b1c59051d34df7988
yq <yqcoconio@gmail.com>
2019-02-26 16:12:00 +0800

    修改了默认账户 连接地址 express static

--- src/server.js
+++ src/server.js
@@ -2,6 +2,8 @@ let express = require('express');
 let app = express();
 
 app.use(express.static("./"));
+app.use('/build', express.static("../build"));
+
 
 app.listen(3000, function() {
     console.log("Dapp server start on port: 3000");
--- truffle.js
+++ truffle.js
@@ -3,9 +3,14 @@ module.exports = {
         development: {
             host: "localhost",
             port: 7545,
-            from: "0x13379Ec77e75012DdE8b0B08B1ff446F4065f52A",
+            from: "0x1c964dBf3Cb12B69b12693E10585092298AAA80a",
             // gas: 4712388, // web3.eth.getBlock("pending").gasLimit
             network_id: "*"
         }
+    },
+    compilers: {
+	solc: {
+      		version: "0.4.24" // ex:  "0.4.20". (Default: Truffle's installed solc)
+    	}
     }
 };
